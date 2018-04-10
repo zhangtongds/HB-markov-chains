b293aa770ee9df81bbdb61e5be5efbe6ed66d4e7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-10 12:16:35 -0700

    Done with make_chains funtcion

--- markov.py
+++ markov.py
@@ -9,12 +9,13 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-    all_text = ""
+    # all_text = ""
     with open(file_path) as text:
-        for line in text:
-            line = line.strip()
-            # words = line.split()
-            all_text += (line + " ")
+        all_text = text.read()
+        # for line in text:
+        #     line = line.strip()
+        #     # words = line.split()
+        #     all_text += (line + " ")
 
     return all_text
 
@@ -43,10 +44,32 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
-
+    # empty dictionary
     chains = {}
 
-    # your code goes here
+    # making a list of words in the string 
+    all_words = text_string.split()
+
+    # giving us the word with it's index 
+    for idx in range(len(all_words)-2):
+        # print idx, all_words[idx + 1],all_words[idx + 2]
+        # makes a key of word and the following word
+       
+
+        # checking to see if key is in dictionary. 
+        #If exits, update value. If it doesn't exists, initiate value with empty list
+        # appends first value to the list 
+        
+        # while idx < len(all_words)-3:
+        key = (all_words[idx], all_words[idx+1])
+        if key in chains:
+            chains[key].append(all_words[idx + 2])
+        else:
+            chains[key] = [all_words[idx + 2]]
+        # print key, all_words[idx+2]
+        # chains.get(key, []).append(all_words[idx+2])
+
+
 
     return chains
 
@@ -63,12 +86,14 @@ def make_text(chains):
 
 input_path = "green-eggs.txt"
 
+# text_string = open_and_read_file(input_path)
+
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 # print input_text
 # Get a Markov chain
-# chains = make_chains(input_text)
-
+chains = make_chains(input_text)
+print chains
 # # Produce random text
 # random_text = make_text(chains)
 
